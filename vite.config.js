8812d888156fae1c17dae05939abeb45cbbd1fad
Leandro Rios <leandro.rios.009@gmail.com>
2022-08-03 20:51:22 -0300

    vueldo el config al estado inicial

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  build: {
-    outDir: './docs'
-  }
+
+  //Agregar para configurar y generar carpeta docs en lugar de dist configurado como base ./ para subir a gh-pages
+
+    //base: "./",
+  // build: {
+   // outDir: './docs'
+ // }
 })
